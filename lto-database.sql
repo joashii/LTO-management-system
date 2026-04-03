8054fbbad6af3a1e508030f4b4a8fd3a7a5fd1ea
Ljiel Saplan <ldsaplan@up.edu.ph>
2026-04-03 14:56:49 +0800

    feat: add lto-database.sql with empty tables

---
+++ lto-database.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE ltodatabase;
+
+USE ltodatabase;
+
+CREATE TABLE driver (
+ 
+);
+
+
+
+CREATE TABLE vehicle (
+ 
+);
+
+
+# DRIVER AND VEHICLE 
+
+CREATE TABLE driver_owns (
+
+);
+
+CREATE TABLE vehicle_has (
+
+);
+
+
+
+# REGISTRATION TABLES
+CREATE TABLE registration (
+ 
+);
+
+CREATE TABLE registration_history (
+ 
+);
+
+CREATE TABLE registration_commits (
+    
+);
+
+# VIOLATION TABLES
+CREATE TABLE violation (
+ 
+);
+
+CREATE TABLE violation_type (
+
+);
+
